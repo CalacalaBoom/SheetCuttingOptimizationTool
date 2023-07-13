d118efcc40c8edbb4613f53db496e8afd0ff3f42
YvonZh <zhangyu19990502@163.com>
2023-07-13 13:34:49 +0800

    listview表头样式修改

--- src/Econo/obj/Debug/net6.0-windows/Views/Welcome.g.i.cs
+++ src/Econo/obj/Debug/net6.0-windows/Views/Welcome.g.i.cs
@@ -54,7 +54,7 @@ namespace Econo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Econo;V1.0.0.0;component/views/welcome.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Econo;component/views/welcome.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Welcome.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
